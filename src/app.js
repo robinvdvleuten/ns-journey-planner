78f0b6af1cd8aafa80518aba115736de28a83366
Robin van der Vleuten <robin@webstronauts.co>
2019-02-13 10:48:36 +0100

    Initialized project with a basic Razzle setup

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Application = () => (
+  <div>Hello, World!</div>
+);
+
+export default Application;
---
+++ src/client.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { hydrate } from 'react-dom';
+import Application from './app';
+
+hydrate(<Application />, window.root);
+
+if (module.hot) {
+  module.hot.accept();
+}
